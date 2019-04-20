bae382cebaa3fafe940e6aeb6a4c0eda638f5e53
poznas <misterdannypl@gmail.com>
2019-04-20 19:59:29 +0200

    Spring Initializr

---
+++ src/main/java/com/bsm/dict/bsmdictionary/BsmDictionaryApplication.java
@@ -0,0 +1,13 @@
+package com.bsm.dict.bsmdictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BsmDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BsmDictionaryApplication.class, args);
+	}
+
+}
