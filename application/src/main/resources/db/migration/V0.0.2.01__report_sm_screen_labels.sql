48bab523cc3515adf83a69d4f01188047c095c4a
poznas <misterdannypl@gmail.com>
2019-05-03 20:35:29 +0200

    report side mission screen labels

---
+++ application/src/main/resources/db/migration/V0.0.2.01__report_sm_screen_labels.sql
@@ -0,0 +1,20 @@
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'PERFORMING_USER', 'EN', 'Performing user');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'PERFORMING_USER', 'PL', 'Czarodziej wykonujący');
+
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'REQUIRED_PROOFS', 'EN', 'Required Proofs');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'REQUIRED_PROOFS', 'PL', 'Wymagane dowody');
+
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'MIN_FILE_SET', 'EN', '(Minimal fileset)');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'MIN_FILE_SET', 'PL', '(Minimalny zestaw plików)');
+
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'INVALID_MEDIA_TYPES', 'EN',
+'Set of media files you choose does not meet proof file type requirements');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'INVALID_MEDIA_TYPES', 'PL',
+'Wybrane pliki nie spełniają wymagań co do typów');
+
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'PHOTO', 'EN', 'Photo');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'PHOTO', 'PL', 'Zdjęcie');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'VIDEO', 'EN', 'Video');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'VIDEO', 'PL', 'Video');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'PHOTO_OR_VIDEO', 'EN', 'Photo or Video');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'PHOTO_OR_VIDEO', 'PL', 'Zdjęcie lub Video');
