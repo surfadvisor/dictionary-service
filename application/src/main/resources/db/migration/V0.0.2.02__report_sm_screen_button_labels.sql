3b9c1d621ea6f653f246a4409181b0ab424fe2cb
poznas <misterdannypl@gmail.com>
2019-05-14 22:20:00 +0200

    report sm screen buttons labels

---
+++ application/src/main/resources/db/migration/V0.0.2.02__report_sm_screen_button_labels.sql
@@ -0,0 +1,5 @@
+
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'ADD_FILE', 'EN', 'Add File');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'ADD_FILE', 'PL', 'Dodaj plik');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'SEND_REPORT', 'EN', 'Report');
+select merge_dict_item('REPORT_SM_SCREEN_LABELS', 'SEND_REPORT', 'PL', 'Raportuj');
