6c7d5d40d4894fa0fcdf60d9a43ef5df6f908a7d
poznas <misterdannypl@gmail.com>
2019-06-05 23:32:01 +0200

    rate side mission report screen labels

---
+++ application/src/main/resources/db/migration/V0.0.2.03__rate_report_screen_labels.sql
@@ -0,0 +1,5 @@
+
+select merge_dict_item('RATE_REPORT_SCREEN_LABELS', 'POST_RATE', 'EN', 'Post Rate');
+select merge_dict_item('RATE_REPORT_SCREEN_LABELS', 'POST_RATE', 'PL', 'Dodaj ocenę');
+select merge_dict_item('RATE_REPORT_SCREEN_LABELS', 'SENDING_RATE', 'EN', 'Sending Rate');
+select merge_dict_item('RATE_REPORT_SCREEN_LABELS', 'SENDING_RATE', 'PL', 'Dodawanie oceny');
