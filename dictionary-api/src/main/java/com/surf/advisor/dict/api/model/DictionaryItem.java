6f4f55239e5285be542708dcd7bd76027c1e9bfa
poznas <misterdannypl@gmail.com>
2019-08-18 17:10:20 +0200

    impl simple dynamoDB literals store connection

--- dictionary-api/src/main/java/com/surf/advisor/dict/api/model/DictionaryItem.java
+++ dictionary-api/src/main/java/com/surf/advisor/dict/api/model/DictionaryItem.java
@@ -4,12 +4,14 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class DictionaryItem {
---
+++ dictionary/src/main/java/com/surf/advisor/dict/dao/DynamoDbDictionaryDao.java
@@ -0,0 +1,37 @@
+package com.surf.advisor.dict.dao;
+
+import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toList;
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
+import com.surf.advisor.dict.api.model.DictionaryItem;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class DynamoDbDictionaryDao implements DictionaryRepository {
+
+  private final DynamoDBMapper mapper;
+
+  @Override
+  public List<DictionaryItem> selectDictionary(String dictKey, String lang) {
+
+    var partitionKey = new DictionaryItemEntity();
+    partitionKey.setDictName(dictKey);
+
+    var queryExpression = new DynamoDBQueryExpression<DictionaryItemEntity>()
+      .withHashKeyValues(partitionKey);
+
+    List<DictionaryItemEntity> results = mapper.query(DictionaryItemEntity.class, queryExpression);
+
+    return ofNullable(results).orElse(emptyList()).stream()
+      .map(entity -> DictionaryItem.builder()
+        .value(ofNullable(entity.getValue()).map(val -> val.get(lang)).orElse(null))
+        .key(entity.getItemName())
+        .lang(lang)
+        .build()
+      ).collect(toList());
+  }
+}
